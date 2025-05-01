748688b2ab98d6e07e359872fc089d3502c5f8b5
ryantakata <ryantakata50@gmail.com>
2025-04-30 20:38:37 -0700

    Implemented API sort call

--- src/api/cheapshark.js
+++ src/api/cheapshark.js
@@ -1,1 +1,12 @@
 // Functions for calling the CheapShark API (fetch game lists, game details, etc.)
+const base_url = "https://www.cheapshark.com/api/1.0";
+
+// sortBy API call
+export const fetchDeals = async(sortBy = 'dealRating') => {
+    try {
+        const response = await fetch(`${base_url}/deals?sortBy=${sortBy}`);
+        return await response.json();
+    } catch(error) {
+        console.error(error);
+    }
+};
