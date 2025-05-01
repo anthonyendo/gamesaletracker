9ee2db466dbbab52f6989d474fb37d2346b5541d
Ryan Takata <ryantakata50@gmail.com>
2025-05-01 07:01:27 -0700

    Page and Search Function

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,17 +3,19 @@ import {useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/HomePage.css';
 import {fetchDeals} from '../api/cheapshark';
-import sortDropdown from '../components/SortDropdown';
 
 function HomePage() {
     const [deals, setDeals] = useState([]);
     const [sort, setSort] = useState('dealRating');
+    const [pageNum, setPageNum] = useState(0);
+    const [search, setSearch] = useState('');
+    const [searchQuery, setSearchQuery] = useState('');
 
     // Fetch deals with async/wait fetch
     useEffect(() => {
         const loadDeals = async () => {
             try {
-                const data = await fetchDeals(sort);
+                const data = await fetchDeals(sort, pageNum, 20, searchQuery);
                 setDeals(data)
             } catch (error) {
                 console.error('Error fetching deals:', error)
@@ -21,25 +23,52 @@ function HomePage() {
         };
 
         loadDeals();
-    }, [sort]);
+    }, [sort, pageNum, searchQuery]);
+
+    // Handle Change Functions
 
     const handleSortChange = (e) => {
         setSort(e.target.value);
+        setPageNum(0);
     };
+
+    const handlePrevPage = () => {
+        if (pageNum > 0) setPageNum(pageNum - 1);
+    };
+
+    const handleNextPage = () => {
+        setPageNum(pageNum + 1);
+    };
+
+    const handleSearchSubmit = (e) => {
+        e.preventDefault();
+        setSearchQuery(search);
+        setPageNum(0);
+    }
 
     return (
         <div>
             <h1>Game Deals</h1>
-            {/* Sort Dropdown Menu */}
-            <div>
-                <label htmlFor="sort">Sort By: </label>
-                <select className='sort' onChange={handleSortChange}>
-                    <option value="dealRating">Deal Rating</option>
-                    <option value="title">Title</option>
-                    <option value="savings">Discount %</option>
-                </select>
-            </div>
+            {/* Game Query Container */}
+            <div className='query-controls'>
 
+                {/* Search Bar */}
+                <form onSubmit={handleSearchSubmit} className='search-bar'>
+                    <input type="text" placeholder='Find Deals...' value={search} onChange={(e) => setSearch(e.target.value)} className='search-text'/>
+                    <button type="submit" className='search-button'>Search</button>
+                </form>
+
+                {/* Sorting Dropdown Menu */}
+                <div className='sort-container'>
+                    <label htmlFor="sort" className='sort-by'>Sort By: </label>
+                    <select className='sort' onChange={handleSortChange}>
+                        <option value="dealRating">Deal Rating (Asc)</option>
+                        <option value="title">Title (A-Z)</option>
+                        <option value="savings">Discount % (Asc)</option>
+                    </select>
+                </div>
+            </div>
+            
             {/* Game Display */}
             <div className="list">
                 {deals.map(deals => (
@@ -63,6 +92,13 @@ function HomePage() {
                 ))}
             </div>
             
+            {/* Page Controls */}
+            <div className='button-container'>
+                <button className='page-button' onClick={handlePrevPage} disabled={pageNum === 0}>Previous</button>
+                <span className='page-number'>Page {pageNum + 1}</span>
+                <button className='page-button' onClick={handleNextPage}>Next</button>
+            </div>
+
         </div>
         
     );
