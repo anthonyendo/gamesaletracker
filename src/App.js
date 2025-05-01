8d9e319a52f849129d84eb40bba5150e024700d0
ryantakata <ryantakata50@gmail.com>
2025-04-30 20:40:29 -0700

    Home Button Navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 // Main component that sets up React Router and routes to HomePage or GameDetailPage
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import GameDetails from './pages/GameDetailPage';
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+    return (
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    );
+  }
 
 function App() {
-
+    return (
+        <Router>
+          <Navbar/>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/game/:id" element={<GameDetails/>} />
+            </Routes>
+        </Router>
+    )
 }
 
 export default App;
