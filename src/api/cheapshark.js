4ee5b874daa065da93c7425d591e00ea5d29ff72
Ryan Takata <ryantakata50@gmail.com>
2025-05-01 07:01:44 -0700

    adjusted api for page number and search

--- src/api/cheapshark.js
+++ src/api/cheapshark.js
@@ -2,9 +2,9 @@
 const base_url = "https://www.cheapshark.com/api/1.0";
 
 // sortBy API call
-export const fetchDeals = async(sortBy = 'dealRating') => {
+export const fetchDeals = async(sortBy = 'dealRating', pageNumber = 0, pageSize = 20, title = '') => {
     try {
-        const response = await fetch(`${base_url}/deals?sortBy=${sortBy}`);
+        const response = await fetch(`${base_url}/deals?sortBy=${sortBy}&pageNumber=${pageNumber}&pageSize=${pageSize}&title=${title}`);
         return await response.json();
     } catch(error) {
         console.error(error);
