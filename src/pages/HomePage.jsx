7dc3161370a1e036d19662ebf94696b713025544
Ryan Takata <ryantakata50@gmail.com>
2025-05-02 06:29:43 -0700

    Container Modification

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,8 @@
 // Full page showing the main game list, search bar, sort dropdown
+import '../styles/HomePage.css';
+import '../App.css';
 import {useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/HomePage.css';
 import {fetchDeals} from '../api/cheapshark';
 
 function HomePage() {
@@ -15,7 +16,7 @@ function HomePage() {
     useEffect(() => {
         const loadDeals = async () => {
             try {
-                const data = await fetchDeals(sort, pageNum, 20, searchQuery);
+                const data = await fetchDeals(sort, pageNum, 21, searchQuery);
                 setDeals(data)
             } catch (error) {
                 console.error('Error fetching deals:', error)
@@ -28,16 +29,18 @@ function HomePage() {
     // Handle Change Functions
 
     const handleSortChange = (e) => {
-        setSort(e.target.value);
+        setSort(e);
         setPageNum(0);
     };
 
     const handlePrevPage = () => {
         if (pageNum > 0) setPageNum(pageNum - 1);
+        window.scrollTo(0, 0);
     };
 
     const handleNextPage = () => {
         setPageNum(pageNum + 1);
+        window.scrollTo(0, 0);
     };
 
     const handleSearchSubmit = (e) => {
@@ -51,45 +54,49 @@ function HomePage() {
             <h1>Game Deals</h1>
             {/* Game Query Container */}
             <div className='query-controls'>
-
                 {/* Search Bar */}
                 <form onSubmit={handleSearchSubmit} className='search-bar'>
                     <input type="text" placeholder='Find Deals...' value={search} onChange={(e) => setSearch(e.target.value)} className='search-text'/>
                     <button type="submit" className='search-button'>Search</button>
                 </form>
-
-                {/* Sorting Dropdown Menu */}
-                <div className='sort-container'>
-                    <label htmlFor="sort" className='sort-by'>Sort By: </label>
-                    <select className='sort' onChange={handleSortChange}>
-                        <option value="dealRating">Deal Rating (Asc)</option>
-                        <option value="title">Title (A-Z)</option>
-                        <option value="savings">Discount % (Asc)</option>
-                    </select>
-                </div>
             </div>
-            
-            {/* Game Display */}
-            <div className="list">
-                {deals.map(deals => (
-                <div key={deals.dealID} className="list-item">
-                    <Link to={`/deals/${deals.cheapestDealID}`} className="img-link">
-                        <img src={deals.thumb} alt={deals.external} className="image" />
-                    <h3>{deals.external}</h3>
-                    </Link>
-                    <div className='details'>
-                        <p>
-                            <Link to={`/deals/${deals.cheapestDealID}`} className='title-link'>
-                                <h3>{deals.title}</h3>
-                            </Link>
-                            Original Price: ${deals.normalPrice}
-                            Discounted Price: ${deals.salePrice}
-                            Savings: {parseFloat(deals.savings).toFixed(1)}%
-                            Deal Rating: {deals.dealRating}
-                        </p>
-                    </div>
+            {/* Page Controls */}
+            <div className='button-container'>
+                <button className='page-button' onClick={handlePrevPage} disabled={pageNum === 0}>Previous</button>
+                <span className='page-number'>Page {pageNum + 1}</span>
+                <button className='page-button' onClick={handleNextPage}>Next</button>
+            </div>
+            {/* Game Display + Sort Buttons */}
+            <div className='content-container'>
+                {/* Sort Buttons */}
+                <div className='sort-container'>
+                    <h3 className='sort-by'>Sort By:</h3>
+                    <button className={`sort-button ${sort === "dealRating" ? "active" : ""}`} onClick={() => handleSortChange("dealRating")}>Deal Rating (Asc)</button>
+                    <button className={`sort-button ${sort === "title" ? "active" : ""}`} onClick={() => handleSortChange("title")}>Title (A-Z)</button>
+                    <button className={`sort-button ${sort === "savings" ? "active" : ""}`} onClick={() => handleSortChange("savings")}>Discount %</button>
                 </div>
-                ))}
+                {/* Game Display */}
+                <div className="list">
+                    {deals.map(deals => (
+                    <div key={deals.dealID} className="list-item">
+                        <Link to={`/deals/${deals.cheapestDealID}`} className="img-link">
+                            <img src={deals.thumb} alt={deals.external} className="image" />
+                        <h3>{deals.external}</h3>
+                        </Link>
+                        <div className='details'>
+                            <p>
+                                <Link to={`/deals/${deals.cheapestDealID}`} className='title-link'>
+                                    <h3>{deals.title}</h3>
+                                </Link>
+                                <span className='original-price'>Original Price: ${deals.normalPrice}</span>
+                                <span className='sale-price'>Discounted Price: ${deals.salePrice}</span>
+                                <span className='savings'>-{parseFloat(deals.savings).toFixed(1)}%</span>
+                                <span className='deal-rating'>Deal Rating: {deals.dealRating}</span>
+                            </p>
+                        </div>
+                    </div>
+                    ))}
+                </div>
             </div>
             
             {/* Page Controls */}
