8f63596748e524fdda9fdb4719c4e1c58d42b822
Ryan Takata <ryantakata50@gmail.com>
2025-05-02 06:30:31 -0700

    Class Name Additions

--- src/App.js
+++ src/App.js
@@ -4,24 +4,28 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 // import GameDetails from './pages/GameDetailPage';
 import { Link } from 'react-router-dom'
+import { FaHome } from 'react-icons/fa';
 
 function Navbar() {
     return (
-      <nav>
-        <Link to="/">Home</Link>
+      <nav className='App'>
+        <Link to="/"><FaHome style={{marginRight:'8px'}}/>Home</Link>
       </nav>
     );
   }
 
 function App() {
     return (
+      <div className='all'>
         <Router>
-          <Navbar/>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                {/* <Route path="/game/:id" element={<GameDetails/>} /> */}
-            </Routes>
-        </Router>
+            <Navbar/>
+              <Routes>
+                  <Route path="/" element={<HomePage />} />
+                  {/* <Route path="/game/:id" element={<GameDetails/>} /> */}
+              </Routes>
+          </Router>
+      </div>
+          
     )
 }
 
